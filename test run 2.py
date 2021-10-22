eb5d70eb767736c2d11c704e6925914a6f515cee
R.P.Nandan <rpnandan@gmail.com>
2021-10-22 09:47:55 +0530

    output of a given number in alphabetical form

---
+++ test run 2.py
@@ -0,0 +1,23 @@
+num = int(input("Enter a number between 0 to 9: "))
+if (num == 0):
+	print("Zero")
+elif (num == 1):
+	print("One")
+elif (num == 2):
+	print("Two")
+elif (num == 3):
+	print("Three")
+elif (num == 4):
+	print("Four")
+elif (num == 5):
+	print("Five")
+elif (num == 6):
+	print("Six")
+elif (num == 7):
+	print("Sevan")
+elif (num == 8):
+	print("Eight")
+elif (num == 9):
+	print("Nine")
+else:
+	print("Wrong Input")
